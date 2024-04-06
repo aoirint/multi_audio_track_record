459ae783e97d26bf89095c9a9972b1215053157b
aoirint <aoirint@gmail.com>
2024-04-06 11:27:08 +0900

    configure argparse

--- multi_audio_track_record/cli.py
+++ multi_audio_track_record/cli.py
@@ -1,13 +1,29 @@
 import logging
+from argparse import ArgumentParser
 from logging import getLogger
+
+from . import __version__ as APP_VERSION
 
 logger = getLogger(__name__)
 
 
 async def main() -> None:
+    parser = ArgumentParser(
+        prog="MultiAudioTrackRecorder",
+    )
+    parser.add_argument(
+        "--version",
+        action="version",
+        version=f"%(prog)s {APP_VERSION}",
+    )
+
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s %(levelname)s %(name)s : %(message)s",
     )
 
-    logger.info("ok")
+    args = parser.parse_args()
+    if hasattr(args, "handler"):
+        args.handler(args)
+    else:
+        parser.print_help()
