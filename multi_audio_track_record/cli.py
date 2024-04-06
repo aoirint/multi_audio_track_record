2658f02abcaf033a3d83a4755f9073cc9792949a
aoirint <aoirint@gmail.com>
2024-04-06 11:22:27 +0900

    configure logging

--- multi_audio_track_record/cli.py
+++ multi_audio_track_record/cli.py
@@ -1,2 +1,13 @@
+import logging
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+
 async def main() -> None:
-    pass
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(levelname)s %(name)s : %(message)s",
+    )
+
+    logger.info("ok")
